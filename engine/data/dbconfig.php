834c8098e1b0ec4ef253b61cbfb0e1822e801e95
FloatingNoda <pkirill@bk.ru>
2020-02-20 21:49:00 +0300

    w

---
+++ engine/data/dbconfig.php
@@ -0,0 +1,21 @@
+<?PHP
+
+define ("DBHOST", "localhost");
+
+define ("DBNAME", "dle13.3");
+
+define ("DBUSER", "root");
+
+define ("DBPASS", "");
+
+define ("PREFIX", "dle");
+
+define ("USERPREFIX", "dle");
+
+define ("COLLATE", "utf8mb4");
+
+define('SECURE_AUTH_KEY', '[0K/+50|C/GSdh>TO-M|m?,LNM~9J|m6Y5)c0%d3K,g(M++LMz!@j1zDJ|E7;N8');
+
+$db = new db;
+
+?>
